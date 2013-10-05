ac8542aa9bba4b6559f554e7c34223ea849d426b
Kbjohnson <kylejson@mail.com>
2013-10-05 05:29:43 -0400

    added guard class defined boundaries fixed chest boundaries

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+//Global Variables
 var DIR_LEFT = 0;
 var DIR_RIGHT = 1;
 var DIR_UP = 2;
 var DIR_DOWN = 3;
-var enemyAmount = 1;
-enchant();
+var chestsAmount = 3;
+var chests = [];
+var guardAmount = chestsAmount;
+var guards = [];
 
+enchant();
 window.onload = function(){
     var game = new Core(320,320);
-    game.preload('images/chara5.png','images/map0.png');
+    game.preload('images/chara5.png','images/map0.png','images/chara6.png');
 
     game.onload = function(){
         var bg = new Sprite(320,320);
@@ -30,7 +34,8 @@ window.onload = function(){
                 this.dir = null;
                 this.anim = null;
                 this.frame = 7;
-                this.movement();
+    
+                this.movement();                
             },
             remove: function(){
 
@@ -46,25 +51,43 @@ window.onload = function(){
                     0,1,2,1]; //down
          
                 this.addEventListener(Event.ENTER_FRAME,function(e){
-                    if(game.input.up){
-                        this.dir = DIR_UP;
-                        this.y -= 4;
-                    } else if(game.input.down){
-                        this.dir = DIR_DOWN;
-                        this.y += 4;
-                    } else if(game.input.left){
-                        this.dir = DIR_LEFT;
-                        this.x -= 4;
-                    } else if(game.input.right){
-                        this.dir = DIR_RIGHT;
-                        this.x += 4;
-                    }
 
+                        if(game.input.up){
+                            this.dir = DIR_UP;
+                            if(this.y > 0)
+                            this.y -= 4;
+                        } else if(game.input.down){
+                            this.dir = DIR_DOWN;
+                            if(this.y < 320 - this.height)
+                            this.y += 4;
+                        } else if(game.input.left){
+                            this.dir = DIR_LEFT;
+                            if(this.x > 0)
+                            this.x -= 4;
+                        } else if(game.input.right){
+                            this.dir = DIR_RIGHT;
+                            if(this.x < 320 - this.width)
+                            this.x += 4;
+                        }
 
-                    if(!game.input.up && !game.input.down && !game.input.left && !game.input.right) this.age = 1;
-                    this.frame = this.anim[this.dir*4 + (this.age%4)];
+                        if(!game.input.up && !game.input.down && !game.input.left && !game.input.right) this.age = 1;
+                        this.frame = this.anim[this.dir*4 + (this.age%4)];
+                    
+                    this.checkChest();
                 });       
+            },
+            checkChest: function(){
+                for(var i=0;i<chestsAmount;i++){
+                    if(this.intersect(chests[i])){
+                        if(chests[i].special == true){
+                            console.log('RIGHT CHEST'); 
+                        }else{
+                            console.log('TRY AGAIN');
+                        }
+                    }     
+                }
             }
+
         });
 
         var Enemy = enchant.Class.create(enchant.Sprite,{
@@ -134,9 +157,62 @@ window.onload = function(){
              });
           }  
         });
+
+        var Chest = enchant.Class.create(enchant.Sprite,{
+            initialize:function(x,y){
+                enchant.Sprite.call(this,16,16);
+                this.image = game.assets['images/map0.png'];
+                this.frame = 25;
+                this.x = x;
+                this.y = y;
+                this.special = false;
+            },
+            remove:function(){
+
+            }
+        });
+
+        //UNFINISHED
+        var Guard = enchant.Class.create(enchant.Sprite,{
+            initialize:function(chestx,chesty,speed){
+                enchant.Sprite.call(this,16,16);
+                this.image = game.assets['images/chara6.png'];
+                this.frame = 5;
+                this.x = 0;
+                this.y = 0;
+                this.speed = speed;
+            
+            },
+            remove:function(){
+
+            },
+            movement:function(chest,speed){
+
+            }
+        });
+
+        //helpers
+        function rand(num){
+            return Math.floor(Math.random() * num);
+        }
         
         var enemy1 = new Enemy(160,160,0);
         var player1 = new Player1();
+        
+        for(var i=0;i<chestsAmount;i++){
+            chests.push(new Chest(rand(304),rand(304)));
+            game.rootScene.addChild(chests[i]);
+            
+            //UNFINISHED
+            for(var j=0;j<guardAmount;j++){
+                guards.push(new Guard(chests[i].x,chests[i].y,2));
+                game.rootScene.addChild(guards[j]);
+            }
+
+        }
+            chests[rand(chestsAmount)].special = true;
+        
+
         game.rootScene.addChild(enemy1);
         game.rootScene.addChild(player1);
         };
